470622d80860e8f09497808aed7bfad553c82e4e
jethrinfox <jethrinfox@gmail.com>
2022-08-21 22:28:13 -0300

    main commit

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,29 @@
+import NextAuth, { type NextAuthOptions } from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+
+// Prisma adapter for NextAuth, optional and can be removed
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { prisma } from "../../../server/db/client"
+import { env } from "../../../env/server.mjs"
+
+export const authOptions: NextAuthOptions = {
+	// Include user.id on session
+	callbacks: {
+		session({ session, user }) {
+			if (session.user) {
+				session.user.id = user.id
+			}
+			return session
+		},
+	},
+	// Configure one or more authentication providers
+	adapter: PrismaAdapter(prisma),
+	providers: [
+		GithubProvider({
+			clientId: process.env.GITHUB_ID,
+			clientSecret: process.env.GITHUB_SECRET,
+		}),
+	],
+}
+
+export default NextAuth(authOptions)
